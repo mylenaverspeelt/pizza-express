3b00c37557288107b21f706fd8b6a917ddf56ff8
mylenaverspeelt <mylena49@hotmail.com>
2024-04-30 09:23:02 -0300

    fetch data for pizza cards on homeage

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,28 +1,23 @@
 import dbConnect from "../../../config/dbConnect.js"
 import Pizza from "../../../models/Pizza.js"
 
-
 export default async function handler(req, res) {
     const { method } = req
 
-    dbConnect()
+    try {
+        await dbConnect()
 
-    if (method === "GET") {
-        const pizza = await Pizza.find()
-        res.status(200).json(pizza)
-    }
-
-    if (method === "POST") {
-        try {
-            const pizza = await Pizza.create(req.body)
-            res.status(201).json()
-
-        } catch (error) {
-            res.status(500).json(error)
+        if (method === "GET") {
+            const pizzas = await Pizza.find()
+            res.status(200).json(pizzas)
         }
+    
+        if (method === "POST") {
+            const pizza = await Pizza.create(req.body)
+            res.status(201).json(pizza)
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Internal Server Error" })
     }
-
-
-
 }
-
